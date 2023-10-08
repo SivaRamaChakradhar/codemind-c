06878f8ed71e33f0c7ced2dfb95330d42f341c10
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-10-08 13:49:52 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if (n%2==1)     {         printf("weird");     }     else if (n%2==0,2>n<5)     {         printf("not weird");     }     else if (n%2==0,6>n<20)     {         printf("weird");     }     else     {         printf("not weird");     } }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if (n%2==1)
+    {
+        printf("weird");
+    }
+    else if (n%2==0,2>n<5)
+    {
+        printf("not weird");
+    }
+    else if (n%2==0,6>n<20)
+    {
+        printf("weird");
+    }
+    else
+    {
+        printf("not weird");
+    }
+}
