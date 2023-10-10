a1d71af00071904d35a4205dff89f386d0a20362
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-10-10 21:13:36 +0530

    #include<stdio.h> int main() {     int p1,p2,p3,p4;     scanf("%d %d %d %d",&p1,&p2,&p3,&p4);     if((p1>=10) && (p2>=10) &&  (p3>=10) && (p4>=10))     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Target.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int p1,p2,p3,p4;
+    scanf("%d %d %d %d",&p1,&p2,&p3,&p4);
+    if((p1>=10) && (p2>=10) &&  (p3>=10) && (p4>=10))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
