3462acb3e7bcad63d3cef2215e201197b0cf695d
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-10-10 20:49:25 +0530

    #include<stdio.h> int main() {     char c;     scanf("%c",&c);     if(c == 'V')     {         printf("Violet");     }     else if(c == 'I')     {         printf("Indigo");     }     else if(c == 'B')     {         printf("Blue");     }     else if(c == 'G')     {         printf("Green");     }     else if(c == 'Y')     {         printf("Yellow");     }     else if(c == 'O')     {         printf("orange");     }     else if(c == 'R')     {         printf("Red");     }     else     {         printf("-1");     } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c == 'V')
+    {
+        printf("Violet");
+    }
+    else if(c == 'I')
+    {
+        printf("Indigo");
+    }
+    else if(c == 'B')
+    {
+        printf("Blue");
+    }
+    else if(c == 'G')
+    {
+        printf("Green");
+    }
+    else if(c == 'Y')
+    {
+        printf("Yellow");
+    }
+    else if(c == 'O')
+    {
+        printf("orange");
+    }
+    else if(c == 'R')
+    {
+        printf("Red");
+    }
+    else
+    { 
+        printf("-1");
+    }
+}
