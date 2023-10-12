de2b548a7bf0ce45557d948984c0a4587604948b
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-10-12 11:18:20 +0530

    #include<stdio.h> int main() {     int x1,y1,x2,y2;     scanf("%d%d%d%d",&x1,&y1,&x2,&y2);     if((x1+y1)<(x2+y2))     {         printf("%d",(x1+y1));     }     else if((x1+y1)==(x2+y2))     {         printf("%d",(x1+y1));     }     else     {         printf("%d",(x2+y2));     } }

---
+++ Interior_Design.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x1,y1,x2,y2;
+    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+    if((x1+y1)<(x2+y2))
+    {
+        printf("%d",(x1+y1));
+    }
+    else if((x1+y1)==(x2+y2))
+    {
+        printf("%d",(x1+y1));
+    }
+    else
+    {
+        printf("%d",(x2+y2));
+    }
+}
