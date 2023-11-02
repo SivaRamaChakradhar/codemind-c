c6ae02491ad4195d6251e1bb5d1fffb361c26a7d
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-11-02 10:43:34 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     if(x%y==0)     {         printf("%d",x/y);     }     else     {         printf("%d",(x/y)+(x%y));     } }

---
+++ Climbing_Up.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(x%y==0)
+    {
+        printf("%d",x/y);
+    }
+    else
+    {
+        printf("%d",(x/y)+(x%y));
+    }
+}
