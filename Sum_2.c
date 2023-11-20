bae8fd183a6b1e0153a647ea782207f3e472c11c
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-11-20 20:24:16 +0530

    #include<stdio.h> int main() {     int a,b,x,y,sum=0,i;     scanf("%d%d%d%d",&a,&b,&x,&y);     for(i=a;i<=b;i++)     {         if(i%x == 0 && i%y !=0 )         {             sum=sum+i;         }     }     printf("%d",sum); }

---
+++ Sum_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x,y,sum=0,i;
+    scanf("%d%d%d%d",&a,&b,&x,&y);
+    for(i=a;i<=b;i++)
+    {
+        if(i%x == 0 && i%y !=0 )
+        {
+            sum=sum+i;
+        }
+    }
+    printf("%d",sum);
+}
