2c8e73fd3828bff2ce754eb7fb10c939c06c5b0a
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-11-24 12:02:47 +0530

    #include<stdio.h> int main() {     int a,b,n,r,rem,i;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i++)     {         n=i;         r=0;         while(n)         {             rem=n%10;             n=n/10;             r=r*10+rem;         }         if(i==r)         {             printf("%d ",i);         }     } }

--- Program_to_print_all_palindromes_in_a_given_range_.c
+++ Program_to_print_all_palindromes_in_a_given_range_.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
-int main(){
-   int num, rem, reverse_num, temp, start, end;
-
-   scanf("%d",&start);
-
-   scanf("%d",&end);
-
-  
-   for(num=start;num<=end;num++){
-      temp=num;
-      reverse_num=0;
-      while(temp){
-         rem=temp%10;
-         temp=temp/10;
-         reverse_num=reverse_num*10+rem;
-      }
-      if(num==reverse_num)
-         printf("%d ",num);
-   }
-   return 0;
+int main()
+{
+    int a,b,n,r,rem,i;
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        n=i;
+        r=0;
+        while(n)
+        {
+            rem=n%10;
+            n=n/10;
+            r=r*10+rem;
+        }
+        if(i==r)
+        {
+            printf("%d ",i);
+        }
+    }
 }
