61fd79b63aa952e6d023513dc4f16f9caecefa22
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-06 17:07:46 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("* ");         }         printf(" ");     }
    }

--- Basic_Pattern_1.1.c
+++ Basic_Pattern_1.1.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int N,i,j;
-    scanf("%d",&N);
-    for(i=1;i<=N;i++)
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=N;j++)
+        for(j=1;j<=n;j++)
         {
             printf("* ");
         }
         printf("
 ");
     }
+    
 }
