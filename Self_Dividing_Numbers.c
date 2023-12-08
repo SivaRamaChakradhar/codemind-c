d9d306e50251df341c38fe3fb4c51763e0063456
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-08 11:53:51 +0530

    #include<stdio.h> int self(int); int main() {     int a,b,i,flag=1,r,t;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i++)     {         flag=1;         t=i;         while(t != 0)         {             r=t%10;             if(r == 0 || i%r != 0)             {                 flag=0;             }             t/=10;         }         if(flag==1)         {             printf("%d ",i);         }     } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -2,23 +2,22 @@
 int self(int);
 int main()
 {
-    int a,b,flag=0,r,i;
+    int a,b,i,flag=1,r,t;
     scanf("%d%d",&a,&b);
     for(i=a;i<=b;i++)
     {
-        flag=0;
-        int t=i;
+        flag=1;
+        t=i;
         while(t != 0)
         {
             r=t%10;
             if(r == 0 || i%r != 0)
             {
-                flag=1;
-
+                flag=0;
             }
             t/=10;
         }
-        if(flag == 0)
+        if(flag==1)
         {
             printf("%d ",i);
         }
