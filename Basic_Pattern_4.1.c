1c4a4e7da469688a6428607e3c696789a509acc6
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-13 18:51:07 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i%2!=0)             {                 printf("0 ");             }             else             {                 printf("1 ");             }         }         printf(" ");     } }

---
+++ Basic_Pattern_4.1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i%2!=0)
+            {
+                printf("0 ");
+            }
+            else
+            {
+                printf("1 ");
+            }
+        }
+        printf("
+");
+    }
+}
