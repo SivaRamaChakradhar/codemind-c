774034eca6c39c3b3f8dbf1d300a25ec5bd366e5
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-19 20:38:56 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(j=i;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     } }

---
+++ Pattern_7.3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("
+");
+    }
+}
