9c8739a334d0fcc275e65721061c75812e01154a
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-21 11:24:48 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j == i || j == n-i+1)             {                 printf("%c",64+i);             }             else             {                 printf(" ");             }         }         printf(" ");     } }

---
+++ Pattern_8.4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j == i || j == n-i+1)
+            {
+                printf("%c",64+i);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+}
