3174ff399df0e6c0e5c72dfed2e7769dd1c70337
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-22 19:33:39 +0530

    #include<stdio.h> int main() {     int n,i,s=0,b,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         s=s+a[i];     }     for(i=0;i<n;i++)     {         b=s/n;         if(a[i] <= b)         {             c++;         }     }     printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s=0,b,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        s=s+a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        b=s/n;
+        if(a[i] <= b)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
