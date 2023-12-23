198db3e411c801d2f4b40a3c6eb4bc71a87dc942
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-23 12:03:47 +0530

    #include<stdio.h> int main() {     int n,i,e=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0&&i%2==0)         {             e++;             break;         }     }     if(e==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,e=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0&&i%2==0)
+        {
+            e++;
+            break;
+        }
+    }
+    if(e==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
