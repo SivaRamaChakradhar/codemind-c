34e3a023880ee409b92e35d061f7b91a4706af8a
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-28 21:00:58 +0530

    #include<stdio.h> int main(){     int n,c=0;     scanf("%d",&n);     while(n!=1)     {         if(n%2==0)         {             n/=2;         }         else if(n%3==0)         {             n/=3;         }         else if(n%5==0)         {             n/=5;         }         else         {             c++;             break;         }     }     if(c==0)     {         printf("Ugly Number");     }     else     {         printf("Not Ugly Number");     } }

---
+++ Ugly_number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int n,c=0;
+    scanf("%d",&n);
+    while(n!=1)
+    {
+        if(n%2==0)
+        {
+            n/=2;
+        }
+        else if(n%3==0)
+        {
+            n/=3;
+        }
+        else if(n%5==0)
+        {
+            n/=5;
+        }
+        else
+        {
+            c++;
+            break;
+        }
+    }
+    if(c==0)
+    {
+        printf("Ugly Number");
+    }
+    else
+    {
+        printf("Not Ugly Number");
+    }
+}
