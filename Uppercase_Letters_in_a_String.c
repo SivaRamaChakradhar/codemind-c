595b7b5ca0c1a581c0310b4bffbb32ac84924005
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2023-12-30 13:47:36 +0530

    #include<stdio.h> int main() {     char s[100];     scanf("%[^ ]s",s);     int i,c=0;     for(i=0;s[i]!=NULL;i++)     {         if(s[i]>='A' && s[i]<='Z')         {             c++;         }     }     printf("%d",c); }

---
+++ Uppercase_Letters_in_a_String.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char s[100];
+    scanf("%[^
+]s",s);
+    int i,c=0;
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]>='A' && s[i]<='Z')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
