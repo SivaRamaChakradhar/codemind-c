60ea65514ebafc206d04560438a9b86d655ca820
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2024-01-06 20:09:38 +0530

    #include<stdio.h> int main() {     int n,i,j,m;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         m=0;         for(j=0;j<n;j++)         {             if(a[j]==a[i])             {                 m++;             }         }         if(m==1)         {             printf("%d",a[i]);         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,m;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        m=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[j]==a[i])
+            {
+                m++;
+            }
+        }
+        if(m==1)
+        {
+            printf("%d",a[i]);
+        }
+    }
+}
